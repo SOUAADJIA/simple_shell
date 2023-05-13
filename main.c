9d3f8cc7c03cd38872824856d02d781cbfea0240
rabiebn <rabie.bnmr@gmail.com>
2023-05-13 23:23:21 +0100

    print prompt and getline

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
+/**
+ * main - main function
+ * @argc: length of argv
+ * @argv: array of strings passed in the command-line
+ * Return: Always 0 (Success) 
+ */
 
 int main(int argc, char **argv)
 {
     char *prompt = "Sshell $ ";
+    char *lineptr;
+    size_t n = 0;
+
+    (void)argc;
+    (void)argv;
 
     printf("%s", prompt);
+    getline(&lineptr, &n, stdin);
+
+    free(lineptr);
+
     return (0);
 }
