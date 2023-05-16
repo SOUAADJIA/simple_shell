e9dbd59a57eea00e78185f9ac0d65340578a018c
SOUAAD OULED-JIA <souadouledjia@gmail.com>
2023-05-16 14:27:44 -0700

    replace getenv()function and add fork

--- build_path.c
+++ build_path.c
@@ -1,54 +1,59 @@
 #include "main.h"
 
-char *build_path(char *command)
+extern char **environ;
+
+char *build_path(char *command) 
 {
     char *path_command, *path_command_copy, *path_token, *file_path;
     int command_len, directory_len;
     struct stat buffer;
+    char **env;
 
-    path_command = getenv("PATH");
-
-    if (path_command)
+    for (env = environ; *env != NULL; env++) 
     {
-        path_command_copy = strdup(path_command);
-        command_len = strlen(command);
+        if (strncmp(*env, "PATH=", 5) == 0) 
+	{
+            path_command = *env + 5;/*point after the PATH= */
+            path_command_copy = strdup(path_command);
+            command_len = strlen(command);
 
-        /* break down the path variable and get all the directories in*/
-        path_token = strtok(path_command_copy, ":");
+            /* break down the path variable and get all the directories */
+            path_token = strtok(path_command_copy, ":");
 
-        while(path_token != NULL)
-        {
-            /* Get the length of the directory*/
-            directory_len = strlen(path_token);
-            /* allocate memory to store the command name + the directory name */
-            file_path = malloc(sizeof(char) * (command_len + directory_len + 2)); /*  2 for / and \0 */
-            /* path= = copy the directory path + concatenate the command  */
-            strcpy(file_path, path_token);
-            strcat(file_path, "/");
-            strcat(file_path, command);
-            strcat(file_path, "\0");
+            while (path_token != NULL) 
+	    {
 
-            /* check if this file path actually exists */
-            if (stat(file_path, &buffer) == 0) /* return 0 means that the file_path is valid*/
-            {
-                free(path_command_copy);/*free the memory allocate by strdup*/
-                return (file_path);
+                /* Get the length of the directory */
+                directory_len = strlen(path_token);
+                /* Allocate memory to store the command name + the directory name */
+                file_path = malloc(sizeof(char) * (command_len + directory_len + 2)); /*  2 for / and \0 */
+                /* Copy the directory path + concatenate the command */
+                strcpy(file_path, path_token);
+                strcat(file_path, "/");
+                strcat(file_path, command);
+                strcat(file_path, "\0");
+
+                /* Check if this file path actually exists */
+                if (stat(file_path, &buffer) == 0) {
+                    free(path_command_copy);
+                    return file_path;
+                } else {
+                    free(file_path);
+                    path_token = strtok(NULL, ":");
+                }
             }
-            else
-            {
-                free(file_path);
-                path_token = strtok(NULL, ":");
+
+            /* If no match, free memory for path_command_copy */
+            free(path_command_copy);
+
+            /* Check if the command itself is an existing path */
+            if (stat(command, &buffer) == 0) {
+                return command;
             }
+
+            return NULL;
         }
-        /* if no match, we return NULL, but we need to free up memory for path_command_copy */
-        free(path_command_copy);
-        /* besote exciting, check if the command itself is a an existing path */
-        if (stat(command, &buffer) == 0)
-        {
-            return (command);
-        }
-        return (NULL);
     }
-    return (NULL);
-}
 
+    return NULL;
+}
--- exec_fun.c
+++ exec_fun.c
@@ -1,18 +1,41 @@
 #include "main.h"
 
-
 void exec_fun(char **argv)
 {
-	char *command = NULL, *updated_command = NULL;
-	int action;
+    char *command = NULL, *updated_command = NULL;
+    int action;
+    pid_t child_pid;
 
-	if (argv)
-	{
-		command = argv[0];
-		/* generate the path to this command before passing it to execve */
-		updated_command = build_path(command);
-		action = execve(updated_command, argv, NULL);
-		if (action == -1)
-			perror("./shell: No such file or directory\n");
-	}
+    if (argv)
+    {
+        command = argv[0];
+        /* generate the path to this command before passing it to execve */
+        updated_command = build_path(command);
+        if (updated_command != NULL)
+        {
+            child_pid = fork();
+            if (child_pid == -1)
+            {
+                perror("fork");
+                return;
+            }
+            else if (child_pid == 0)
+            {
+                action = execve(updated_command, argv, NULL);
+                if (action == -1)
+                    perror("./shell: No such file or directory\n");
+                exit(EXIT_FAILURE); /*Child process exits after execution or on failure*/
+            }
+            else
+            {
+                int status;
+                
+                waitpid(child_pid, &status, 0); /* Parent process waits for the child to finish*/
+            }
+        }
+        else
+        {
+            printf("Command '%s' does not exist.\n", command);
+        }
+    }
 }
